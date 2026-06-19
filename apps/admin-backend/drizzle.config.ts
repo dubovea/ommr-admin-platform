2d0c7aba4e7d2ebcf413ab7f682aefd05e249315
EDubov <Edubov@project.ibs.ru>
2026-06-19 11:56:16 +0500

    removing changes from drizzle

--- apps/admin-backend/drizzle.config.ts
+++ apps/admin-backend/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 import { getDatabaseConfig } from "./src/config/database.ts";
 
 export default defineConfig({
-  schema: "apps/admin-backend/src/db/schema.ts",
-  out: "apps/admin-backend/drizzle",
+  schema: "./src/db/schema.ts",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: getDatabaseConfig(),
   verbose: true,
